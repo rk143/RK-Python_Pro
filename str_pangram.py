04c30cd67b82c0285781c3313231e67333cd938c
Rajeshkannan <37392806+rk143@users.noreply.github.com>
2019-07-30 16:07:11 +0530

    Create str_pangram.py

---
+++ str_pangram.py
@@ -0,0 +1,12 @@
+def ispangram(str): 
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabet: 
+        if char not in str.lower(): 
+            return False
+  
+    return True
+string =input()
+if(ispangram(string) == True): 
+    print("yes") 
+else: 
+    print("no")
